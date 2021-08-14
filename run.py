f2f0873110f64e70ae5e4734edd8f450a05b6acd
Erhe <75741783+w16692926717@users.noreply.github.com>
2021-08-14 14:36:04 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 import os
 import time
-os.system("python3 ./王XX.py")
+os.system("python3 ./name.py")
 time.sleep(60)           # 两个人连续打卡时间的间隔建议大于5s以上
-os.system("python3 ./李XX.py")
+os.system("python3 ./name2.py")
 time.sleep(60)
-os.system("python3 ./张XX.py")
+os.system("python3 ./name3.py")
 time.sleep(60)
 # ··· ···
-os.system("python3 ./人名.py")
+os.system("python3 ./namen.py")
 time.sleep(60)
 '''
-os.system("python3 ./人名.py")
+os.system("python3 ./namen+1.py")
 time.sleep(60)
  ··· ··· 无限循环往下继续写，多少人打卡就写多少套
 '''
